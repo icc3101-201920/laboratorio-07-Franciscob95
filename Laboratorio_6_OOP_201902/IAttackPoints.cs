d361f77aaa4174eed295be6746ce6d04d071fd49
Franciscob95 <fjbarros1@miuandes.cl>
2019-10-11 09:58:09 -0300

    Revisión metodos

---
+++ Laboratorio_6_OOP_201902/IAttackPoints.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_6_OOP_201902
+{
+    interface IAttackPoints
+    {
+        int[] GetAttackPoints(Enums.EnumType line = Enums.EnumType.None);
+    }
+}
